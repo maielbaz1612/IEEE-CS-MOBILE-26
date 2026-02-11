75de8296023bb4591a953584dcc79a060b6b0bbb
maielbaz1612 <maim61366@gmail.com>
2026-02-11 21:18:03 +0200

    task-1

--- Task-01/palindrome.dart
+++ Task-01/palindrome.dart
@@ -1,15 +1,14 @@
 import 'dart:io';
 
 void main() {
-  
-  String?word = stdin.readLineSync();
+  String word = stdin.readLineSync()!;
 
   String revWord = word.split('').reversed.join('');
-  
+
   if (word == revWord){
     print("string is a palindrome");
   }else{
     print("string is not a palindrome");
   }
-  
+ 
 }
